8f1a629c6f5371d5fbf36319bb7ae1fb0211c111
kamil-jodlowski <kamil.jodlowski.work@gmail.com>
2024-06-20 14:49:14 +0200

    Update queries.sql

--- Module8-Using_NULL/queries.sql
+++ Module8-Using_NULL/queries.sql
@@ -1,119 +1,70 @@
-/* Query 1:List the films where the yr is 1962 [Show id, title] */
+/* Query 1:List the teachers who have NULL for their department. */
 
-SELECT id, title
-FROM  movie 
-WHERE yr = 1962
-
-
-/* Query 2: Give year of 'Citizen Kane'.*/
-
-SELECT yr
-FROM movie
-WHERE title = 'Citizen Kane'
+SELECT name 
+FROM teacher 
+WHERE dept IS NULL;
 
 
-/* Query 3: List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). 
-  Order results by year.*/
+/* Query 2: Note the INNER JOIN misses the teachers with no department and the departments with no teacher..*/
 
-SELECT id, title, yr 
-FROM movie 
-WHERE title LIKE ('%Star Trek%')
-ORDER BY yr
+SELECT teacher.name, dept.name
+FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
 
-/* Query 4: What id number does the actor 'Glenn Close' have?*/
+
+/* Query 3: Use a different JOIN so that all teachers are listed.*/
+
+SELECT teacher.name, dept.name
+FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+
+/* Query 4: Use a different JOIN so that all departments are listed. */
   
-SELECT id 
-FROM actor
-WHERE name = 'Glenn Close'
+SELECT teacher.name, dept.name
+ FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id)
 
-/* Query 5: What is the id of the film 'Casablanca' */
+/* Query 5:Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. 
+Show teacher name and mobile number or '07986 444 2266' */
   
-SELECT id
-FROM movie
-WHERE title = 'Casablanca'
+SELECT name , COALESCE (mobile, '07986 444 2266')
+FROM teacher 
 
-/* Query 6: Obtain the cast list for 'Casablanca'.
-what is a cast list?
-Use movieid=11768, (or whatever value you got from the previous question) */
+/* Query 6: Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. 
+Use the string 'None' where there is no department. */
   
-SELECT name
-FROM actor
-JOIN casting ON casting.actorid = actor.id
-JOIN movie ON casting.movieid = movie.id
-WHERE movie.title = 'Casablanca';
+SELECT teacher.name, COALESCE(dept.name, 'None')
+FROM teacher
+LEFT JOIN dept ON teacher.dept = dept.id 
 
-/* Query 7: Obtain the cast list for the film 'Alien' */
+/* Query 7: Use COUNT to show the number of teachers and the number of mobile phones. */
   
-SELECT name
-FROM actor
-JOIN casting ON casting.actorid = actor.id
-JOIN movie ON casting.movieid = movie.id
-WHERE movie.title = 'Alien';
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher
 
-/* Query 8: List the films in which 'Harrison Ford' has appeared.*/
+/* Query 8: Use COUNT and GROUP BY dept.name to show each department and the number of staff. 
+Use a RIGHT JOIN to ensure that the Engineering department is listed.*/
   
-SELECT title
-FROM actor
-JOIN casting ON casting.actorid = actor.id
-JOIN movie ON casting.movieid = movie.id
-WHERE name = 'Harrison Ford';
+SELECT dept.name, COUNT(teacher.name)
+FROM teacher
+RIGHT JOIN dept ON dept.id = teacher.dept
+GROUP BY dept.name
 
-/* Query 9: List the films where 'Harrison Ford' has appeared - but not in the starring role. 
-[Note: the ord field of casting gives the position of the actor. 
-If ord=1 then this actor is in the starring role]*/
+/* Query 9: Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.*/
   
-SELECT title
-FROM actor
-JOIN casting ON casting.actorid = actor.id
-JOIN movie ON casting.movieid = movie.id
-WHERE name = 'Harrison Ford' AND ord <> 1 ;
-
-/* Query 10: List the films together with the leading star for all 1962 films.*/
+SELECT name, CASE WHEN dept = 1 OR dept =2 
+                                             THEN 'Sci'
+                                              ELSE 'Art'
+                                              END
+FROM teacher ;
   
-SELECT stadium, COUNT(gtime)
-SELECT title, name
-FROM actor
-JOIN casting ON casting.actorid = actor.id
-JOIN movie ON casting.movieid = movie.id
-WHERE yr = '1962' AND ord = 1 ;
-
-/* Query 11: Which were the busiest years for 'Rock Hudson', 
-show the year and the number of movies he made each year for any year in which he made more than 2 movies.*/
+/* Query 10: Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in 
+dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.*/
   
-SELECT yr,COUNT(title)
-FROM movie 
-JOIN casting ON movie.id=movieid
-JOIN actor   ON actorid=actor.id
-WHERE name='Rock Hudson'
-GROUP BY yr
-HAVING COUNT(title) > 2
-
-/* Query 12: List the film title and the leading actor for all of the films 'Julie Andrews' played in.*/
-  
-SELECT title, name
-FROM casting 
-JOIN actor ON actor.id = casting.actorid
-JOIN movie ON movie.id = casting.movieid
-WHERE movieid IN (SELECT movieid FROM casting
-WHERE actorid IN (
-SELECT id FROM actor
-WHERE name='Julie Andrews'))
-AND ord = 1 ;
-
-/* Query 13: Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles.*/
-  
-SELECT name
-FROM casting
-JOIN actor ON actor.id = casting.actorid 
-WHERE ord = 1
-GROUP BY name
-HAVING COUNT(ord) >= 15;
-
-/* Query 14: List the films released in the year 1978 ordered by the number of actors in the cast, then by title.*/
-  
-SELECT title, COUNT(actorid)
-FROM casting
-JOIN movie ON movie.id = casting.movieid
-WHERE yr = 1978
-GROUP BY movieid 
-ORDER BY COUNT(actorid) DESC , title
+SELECT name, CASE WHEN dept = 1 OR dept =2 
+                                             THEN 'Sci'
+                                              WHEN dept = 3
+                                              THEN 'Art'
+                                              ELSE 'None'
+                                              END
+FROM teacher;
